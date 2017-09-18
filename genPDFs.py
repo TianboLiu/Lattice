6fb27a0e1b9a8e0869ac4150d4cf15593e3b3687
Tianbo Liu <tl190@grads-dd.internal.phy.duke.edu>
2017-09-18 17:42:15 -0400

    A

--- genPDFs.py
+++ genPDFs.py
@@ -12,15 +12,15 @@ def genPDFs(filename, pdfset, flavor, Q, nlines):
         f.write("%d\t" %i)
         f.write("\n")
     pdflist = np.arange((nlines+2)*1000, dtype=np.float64).reshape(1000, nlines+2)
-    xlist = 10**np.linspace(np.log10(0.515e-7), 0, 1000)
+    xlist = 10**np.linspace(np.log10(2.035e-7), 0, 1000)
     for i in range(1000):
-    pdflist[i,0] = xlist[i]
+        pdflist[i,0] = xlist[i]
     for id in range(nlines+1):
         pdf = lhapdf.mkPDF(pdfset, id)
         for i in range(1000):
             pdflist[i, id+1] = pdf.xfxQ(flavor, xlist[i], Q) / xlist[i];
     for i in range(1000):
-        for j in range(102):
+        for j in range(nlines+2):
             f.write("%.6E\t" % pdflist[i,j])
             f.write("\n")
     f.close()
@@ -29,9 +29,10 @@ def genPDFs(filename, pdfset, flavor, Q, nlines):
 
 if __name__=="__main__":
     if len(sys.argv) < 6:
+        print "./genPDFs <filename> <pdfset> <flavor> <Q> <nlines>"
         sys.exit()
     else:
-        genPDFs(argv[1], argv[2], argv[3], argv[4], argv[5])
+        genPDFs(sys.argv[1], sys.argv[2], int(sys.argv[3]), float(sys.argv[4]), int(sys.argv[5]))
         print "Bye!"
         
 
